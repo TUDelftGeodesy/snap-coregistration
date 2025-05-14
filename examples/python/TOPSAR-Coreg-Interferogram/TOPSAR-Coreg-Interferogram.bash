b5235a4cda0b8ff0bdc1ebb627aea6cb06447c64
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-05-14 16:11:28 +0200

    small improvements

--- examples/python/TOPSAR-Coreg-Interferogram/TOPSAR-Coreg-Interferogram.bash
+++ examples/python/TOPSAR-Coreg-Interferogram/TOPSAR-Coreg-Interferogram.bash
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=01:00:00
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --partition=normal
 
 module load snap
@@ -13,7 +13,7 @@ MOTHER_PATH=${DATA_DIR}/IW_SLC__1SDV_VVVH/20250416/S1A_IW_SLC__1SDV_20250416T172
 DAUGHTER_PATH=${DATA_DIR}/IW_SLC__1SDV_VVVH/20250428/S1A_IW_SLC__1SDV_20250428T172547_20250428T172614_058960_074FA0_4F0C.zip
 AOI_WKT="POLYGON ((5.111186 52.990841, 5.111186 53.31203, 4.704692 53.31203, 4.704692 52.990841, 5.111186 52.990841))"
 
-python3.10 TOPSAR-Coreg-Interferogram.py \
+time python3.10 TOPSAR-Coreg-Interferogram.py \
   -q ${SLURM_CPUS_PER_TASK} \
   --mother-path "${MOTHER_PATH}" \
   --daughter-path "${DAUGHTER_PATH}" \
